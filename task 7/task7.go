e7f372f810a6efebc30ec59fd08765436dc6377e
danclear115-coder <danclear115@gmail.com>
2026-08-14 19:58:22 +0300

    added new task

---
+++ task 7/task7.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func reverse(x int) int {
+
+	result, isMin := 0, false
+
+	if x < 0 {
+		x = x * -1
+		isMin = true
+	}
+	
+	for x > 0 {
+		num := x % 10
+		result = result * 10 + num
+		x = x / 10	
+	}
+
+	if result > 2147483647 || result < -2147483648 {
+		return 0
+	}
+
+	if isMin {
+		return result * -1
+	} else {
+		return result
+	}
+	
+}
+
+func main() {
+	fmt.Println(reverse(-123))
+}
